0d31f93126dea8c851f084c87b6009072032854e
Velezer <asvelezer@gmail.com>
2022-04-25 23:19:59 +0700

    chore: add null test case

--- src/kvf.spec.ts
+++ src/kvf.spec.ts
@@ -6,36 +6,41 @@ interface testtype {
     name: string
     num: number
     yes: boolean
+    date: Date
+    null: null
 }
 
 beforeAll(() => kvf = new KVF('--temp--'))
 afterAll(() => kvf.clearAll())
 
 describe('kvf without ttl', () => {
-    it('save string', () => {
-        const key = 'key'
-        const value = 'value'
-        kvf.set(key, value)
-        const data = kvf.get(key) as string
-        expect(data).toEqual(value)
-    })
     it('save testtype', () => {
-        const keytest = 'key-test'
+        const key = 'key-test'
         const value: testtype = {
             name: 'name',
             num: 12,
-            yes: false
+            yes: false,
+            date: new Date(),
+            null: null
         }
-        kvf.set(keytest, value)
-        const data = kvf.get(keytest) as testtype
+        kvf.set(key, value)
+        const data = kvf.get(key) as testtype
         expect(data).toEqual(value)
+        expect(data.date.getDate()).toEqual(new Date().getDate())
     })
     it('save new Date()', () => {
-        const keytest = 'key-date'
+        const key = 'key-date'
         const value = new Date()
-        kvf.set(keytest, value)
-        const data = kvf.get(keytest) as Date
+        kvf.set(key, value)
+        const data = kvf.get(key) as Date
         expect(new Date(data)).toEqual(value)
+    })
+    it('save null', () => {
+        const key = 'key-null'
+        const value = null
+        kvf.set(key, value)
+        const data = kvf.get(key) as null
+        expect(data).toEqual(value)
     })
 })
 
