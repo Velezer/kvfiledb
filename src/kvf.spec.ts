2cca2a77046cfa78f30b4ddf0e4e74870cba5969
Velezer <asvelezer@gmail.com>
2022-04-26 02:53:09 +0700

    feat: add ability to add non primitive type for future use

--- src/kvf.spec.ts
+++ src/kvf.spec.ts
@@ -2,12 +2,17 @@ import { KVF } from './kvf';
 
 let kvf: KVF
 
-interface testtype {
-    name: string
-    num: number
-    yes: boolean
-    date: Date
+interface primitive {
+    string: string
+    number: number
+    boolean: boolean
     null: null
+}
+interface nonprimitive {
+    date: Date
+    another: {
+        date: Date
+    }
 }
 
 beforeAll(() => kvf = new KVF('--temp--'))
@@ -30,33 +35,58 @@ describe('kvf with ttl', () => {
     })
 })
 describe('kvf without ttl', () => {
-    it('save testtype', () => {
-        const key = 'key-test'
-        const value: testtype = {
-            name: 'name',
-            num: 12,
-            yes: false,
-            date: new Date(),
+    it('save object primitive', () => {
+        const key = 'key-obj-primitive'
+        const value: primitive = {
+            string: 'name',
+            number: 12,
+            boolean: false,
             null: null
         }
         kvf.set(key, value)
-        const data = kvf.get(key) as testtype
+        const data = kvf.get(key) as primitive
         expect(data).toEqual(value)
-        expect(data.date.getDate()).toEqual(new Date().getDate())
     })
-    it('save new Date()', () => {
-        const key = 'key-date'
-        const value = new Date()
-        kvf.set(key, value)
-        const data = kvf.get(key) as Date
-        expect(new Date(data)).toEqual(value)
-    })
+
     it('save null', () => {
         const key = 'key-null'
         const value = null
         kvf.set(key, value)
         const data = kvf.get(key) as null
         expect(data).toEqual(value)
+    })
+})
+
+describe('kvf non primitive type', ()=>{
+    it('save object nonprimitive', () => {
+        const key = 'key-obj-date'
+        const value: nonprimitive = {
+            date: new Date(),
+            another:{date: new Date()}
+        }
+        kvf.set(key, value)
+        const data = kvf.get(key) as nonprimitive
+        expect(data.date.getDate()).toEqual(new Date().getDate())
+        expect(data.date.getDay()).toEqual(new Date().getDay())
+        expect(data.date.getHours()).toEqual(new Date().getHours())
+        expect(data.date.getMonth()).toEqual(new Date().getMonth())
+        expect(data.date.getFullYear()).toEqual(new Date().getFullYear())
+        expect(data.another.date.getDate()).toEqual(new Date().getDate())
+        expect(data.another.date.getDay()).toEqual(new Date().getDay())
+        expect(data.another.date.getHours()).toEqual(new Date().getHours())
+        expect(data.another.date.getMonth()).toEqual(new Date().getMonth())
+        expect(data.another.date.getFullYear()).toEqual(new Date().getFullYear())
+    })
+    it('save new Date()', () => {
+        const key = 'key-date'
+        const value = new Date()
+        kvf.set(key, value)
+        const data = kvf.get(key) as Date
+        expect(data.getDate()).toEqual(new Date().getDate())
+        expect(data.getDay()).toEqual(new Date().getDay())
+        expect(data.getHours()).toEqual(new Date().getHours())
+        expect(data.getMonth()).toEqual(new Date().getMonth())
+        expect(data.getFullYear()).toEqual(new Date().getFullYear())
     })
 })
 
--- src/kvf.ts
+++ src/kvf.ts
@@ -1,6 +1,26 @@
 import fs from 'fs'
 import path from 'path'
 
+function iterateTransform(obj: object): any {
+    for (const key in obj) {
+        if (key === '__single__') break
+        const element = Object(obj)[key]
+        if (element instanceof Date) {
+            Object(obj)[key] = { __type__: 'date', __value__: element }
+            continue
+        }
+        if (typeof element === 'object') {
+            return transform(Object(obj)[key])
+        }
+    }
+}
+
+function transform(obj: object): any {
+    if (obj instanceof Date) obj = Object({ __single__: { __type__: 'date', __value__: obj } })
+    iterateTransform(obj)
+    return obj
+}
+
 export class KVF {
     private path: string
     constructor(path: string = '_temp_') {
@@ -13,12 +33,13 @@ export class KVF {
      * @param data can be anything
      * @param ttl in ms
      */
-    async set<T>(key: string, data: T, ttl?: number) {
+    async set<T>(key: string, data: T | object, ttl?: number) {
+        data = transform(data as object)
         const p = path.join(this.path, key)
         try {
             fs.writeFileSync(p, JSON.stringify(data, null, '\t'), { encoding: 'utf8' })
         } catch (err: any) {
-            if (err.code == 'ENOENT'){
+            if (err.code == 'ENOENT') {
                 if (!fs.existsSync(this.path)) {// create folder if doesn't exist
                     fs.mkdirSync(this.path, { recursive: true })
                     fs.writeFileSync(p, JSON.stringify(data, null, '\t'), { encoding: 'utf8' })
@@ -41,13 +62,11 @@ export class KVF {
         if (!fs.existsSync(p)) return null
 
         const data = fs.readFileSync(p, { encoding: 'utf8' })
-        return JSON.parse(data, (k, v) => {
-            if (typeof v === 'string') {
-                const date = new Date(v)
-                if (!isNaN(Number(date))) return date
-            }
+        const parsed = JSON.parse(data, (k, v) => {
+            if (v?.__type__ === 'date') return new Date(v.__value__)
             return v
         })
+        return parsed?.__single__ || parsed
     }
 
     /**
